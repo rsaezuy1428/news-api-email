339c3f40a332f5fffa807d76e5d49c2ea51cbae1
rsaez1428 <rafaelsaez@yahoo.com>
2023-12-03 09:50:29 -0300

    Initial Commit.

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ import requests
 
 from send_email import send_email
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-11-03&sortBy=publishedAt&apiKey=0251d59d30084966987c3833adf218b6"
+topic = "Amazon"
+country = "us"
+category = "business"
+api_key = "0251d59d30084966987c3833adf218b6"
+url = "https://newsapi.org/v2/top-headlines?" \
+    f"country={country}&" \
+    f"category={category}&" \
+    f"topic={topic}&" \
+    f"apiKey={api_key}&" \
+    "language=en"
 
 request = requests.get(url)
 
 content = request.json()
 
-send_email(content['articles'][0]['title'].encode('utf-8'))
+subject = "Subject: Today's news" + "\n"
+body = ""
+for article in content['articles'][:10]:
+    if article['title'] is not None and article['description'] is not None:
+        body = body + article['title'] + "\n" + article['description'] + "\n" + article['url'] + 2*"\n"
+
+send_email(subject.encode('utf-8') + body.encode('utf-8'))
